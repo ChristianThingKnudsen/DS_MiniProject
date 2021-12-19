a797fe39fcbc6aced0f952c8d5fc9781fd6a6fa4
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-12-19 18:33:50 +0100

    2k 10mb added

--- post-script.py
+++ post-script.py
@@ -1,8 +1,9 @@
 import requests
 import time
+import sys
 
 file_names = ["10KB.bin", "100KB.bin", "1MB.bin", "10MB.bin", "100MB.bin"]
-file_name = file_names[4]
+file_name = file_names[3]
 
 replicas = [2,3,4]
 n_replica = replicas[0]
@@ -17,13 +18,16 @@ files =  {
         "file": file
 }
 for i in range(100):
-    time.sleep(40)
+    # time.sleep(40)
     file.seek(0,0)
     t1 = time.time()
     response = requests.post(url, files=files, data = body)
     f = open("./measurements/HDFS/HDFS_results_"+str(n_replica)+"k_"+ file_name.split(".")[0] + ".csv", "a")
     f.write(str(time.time()-t1) + "\n")
     f.close()
-    print("completed request for " +file_name+ ": "+ str(i))
+    # print("completed request for " +file_name+ ": "+ str(i))
+    sys.stdout.write("completed request for " +file_name+ " "+ "time: "+ str(time.time()-t1)+ " iteration: "+ str(i) + "\n")  # same as print
+    sys.stdout.flush()
+    time.sleep(5)
             
 print("Done John")
