6ef20d0e0805760f1bd0e2f20a3c0b65033454b7
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-12-19 14:30:47 +0100

    add

--- post-script.py
+++ post-script.py
@@ -2,10 +2,10 @@ import requests
 import time
 
 file_names = ["10KB.bin", "100KB.bin", "1MB.bin", "10MB.bin", "100MB.bin"]
-file_name = file_names[3]
+file_name = file_names[0]
 
 replicas = [2,3,4]
-n_replica = replicas[2]
+n_replica = replicas[0]
 
 url = "http://192.168.0.100:5000/files"
 body = {
@@ -17,7 +17,7 @@ files =  {
         "file": file
 }
 for i in range(100):
-    time.sleep(7)
+    time.sleep(1)
     file.seek(0,0)
     t1 = time.time()
     response = requests.post(url, files=files, data = body)
