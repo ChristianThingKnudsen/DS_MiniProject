97946fc355335d0a608897fed17086e751a2ae52
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-12-21 00:43:53 +0100

    add

--- get-script.py
+++ get-script.py
@@ -1,7 +1,7 @@
 import requests
 import time
 import sys
-#EC_b_1l_dec_1MB
+#EC_b_2l_dec_1MB
 # Files 
 file_names = ["10KB.bin", "100KB.bin", "1MB.bin", "10MB.bin", "100MB.bin"]
 file_name = file_names[2]
@@ -14,7 +14,7 @@ for i in range(100):
     t1 = time.time()
     response = requests.get(url)
     # f = open("./measurements/"+storage_type+"/"+storage_type+"_access_"+ file_name.split(".")[0] + ".csv", "a")
-    f = open("./measurements/"+storage_type+"/"+storage_type+"_access_"+"1l_"+ file_name.split(".")[0] + ".csv", "a")
+    f = open("./measurements/"+storage_type+"/"+storage_type+"_access_"+"2l_"+ file_name.split(".")[0] + ".csv", "a")
     f.write(str(time.time()-t1) + "\n")
     f.close()
     sys.stdout.write("completed request for " +file_name+ " "+ "time: "+ str(time.time()-t1)+ " iteration: "+ str(i) + "\n")  
