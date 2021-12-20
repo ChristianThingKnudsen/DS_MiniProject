7d966520ded4d184a5087f95bfe4ddc4a1dc27c8
Christian Thing Knudsen <ct-1000@hotmail.com>
2021-12-20 09:36:05 +0100

    Some RAID1 added

--- post-script.py
+++ post-script.py
@@ -2,32 +2,49 @@ import requests
 import time
 import sys
 
+# Files 
 file_names = ["10KB.bin", "100KB.bin", "1MB.bin", "10MB.bin", "100MB.bin"]
-file_name = file_names[4]
-
+file_name = file_names[2]
+# Replicas
 replicas = [2,3,4]
-n_replica = replicas[2]
+n_replica = replicas[0]
+# Storage types
+storage_types = ["HDFS", "RAID1", "EC_RS"]
+storage_type = storage_types[1]
+
 
 url = "http://192.168.0.100:5000/files"
 body = {
     "n_replicas": n_replica,
-    "storage": "HDFS"
+    "storage": storage_type,
+    "n_stripes": 1,  # Only for RAID1
 }
 file = open("../"+file_name,"rb")
 files =  {
         "file": file
 }
-for i in range(100):
-    # time.sleep(40)
+for i in range(100): 
     file.seek(0,0)
-    t1 = time.time()
-    response = requests.post(url, files=files, data = body)
-    f = open("./measurements/HDFS/HDFS_results_"+str(n_replica)+"k_"+ file_name.split(".")[0] + ".csv", "a")
-    f.write(str(time.time()-t1) + "\n")
-    f.close()
-    # print("completed request for " +file_name+ ": "+ str(i))
-    sys.stdout.write("completed request for " +file_name+ " "+ "time: "+ str(time.time()-t1)+ " iteration: "+ str(i) + "\n")  # same as print
-    sys.stdout.flush()
-    time.sleep(70)
-            
+    if storage_type == "HDFS":
+        t1 = time.time()
+        response = requests.post(url, files=files, data = body)
+        f = open("./measurements/HDFS/HDFS_results_"+str(n_replica)+"k_"+ file_name.split(".")[0] + ".csv", "a")
+        f.write(str(time.time()-t1) + "\n")
+        f.close()
+        sys.stdout.write("completed request for " +file_name+ " "+ "time: "+ str(time.time()-t1)+ " iteration: "+ str(i) + "\n")  # same as print
+        sys.stdout.flush()
+        time.sleep(70)
+    elif storage_type == "RAID1":
+        t1 = time.time()
+        response = requests.post(url, files=files, data = body)
+        f = open("./measurements/RAID1/RAID1_results_"+str(n_replica)+"k_"+ file_name.split(".")[0] + ".csv", "a")
+        f.write(str(time.time()-t1) + "\n")
+        f.close()
+        sys.stdout.write("completed request for " +file_name+ " "+ "time: "+ str(time.time()-t1)+ "Type: " + storage_type + " iteration: "+ str(i) + "\n")  # same as print
+        sys.stdout.flush()
+    elif storage_type == "EC_RS":
+        print("EC_RS")
+    else:
+        print("Unnown storage type")
+        break        
 print("Done John")
